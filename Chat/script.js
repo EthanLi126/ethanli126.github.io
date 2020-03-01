5f45176f2ec1656453e40a26d1ac4d6a3e55c566
Justin lo <justin.zihing.lo@gmail.com>
2020-03-01 18:39:48 -0500

    backend

--- Chat/script.js
+++ Chat/script.js
@@ -2,8 +2,6 @@
 
   window.addEventListener('offline', function(e) { alert('Sorry, but there is no internet connection. Please connect to the internet and try again.'); });
   //custom spam stopper :>
-  var spam_counter;
-  var timeout;
 
   //firebase code
   var config = {
@@ -212,7 +210,6 @@
       alert("Calm Down! Every message you send costs money you know. Please be considerate for us poor developers.")
       setTimeout(() => {}, 2000);
     }
-    spam_counter = 0;
   },0500);
   
   searchFilter.init();
