5e72340b0c13140149515ca613ebdb2f5ee09097
EthanLi126 <ethan.li@ucc.on.ca>
2020-03-01 19:00:42 -0500

    yesa

--- Chat/script.js
+++ Chat/script.js
@@ -1,10 +1,8 @@
 (function(){
-<<<<<<< HEAD
 
-=======
->>>>>>> c1ec49927abd55d41a80a0d17917a0f54dd24b31
   window.addEventListener('offline', function(e) { alert('Sorry, but there is no internet connection. Please connect to the internet and try again.'); });
   //custom spam stopper :>
+
 
   //firebase code
   var config = {
@@ -215,6 +213,7 @@
       alert("Calm Down! Every message you send costs money you know. Please be considerate for us poor developers.")
       setTimeout(() => {}, 2000);
     }
+
   },0500);
   
   searchFilter.init();
